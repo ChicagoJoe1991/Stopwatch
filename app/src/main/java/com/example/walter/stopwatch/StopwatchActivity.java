7e6ebcd42ad7d8e98cf7c7b2a7de459cd1beca9d
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-17 15:36:46 -0600

    create runTimer() method

--- app/src/main/java/com/example/walter/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/walter/stopwatch/StopwatchActivity.java
@@ -8,45 +8,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private boolean running;
+    private int seconds = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stopwatch, menu);
-        return true;
+    protected void onClickStart(){
+        running = true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onClickStop(){
+        running = false;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    protected void onClickReset(){
+        running = false;
+        seconds = 0;
+
+    }
+
+    private void runTimer(){
+        //get the TextView
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        int hours = seconds/3600;
+        int minutes = seconds/60;
+        int secs = seconds%60;
+
+        String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+        timeView.setText(time);
+        if(running) seconds++;
+
+    }
+
 }
