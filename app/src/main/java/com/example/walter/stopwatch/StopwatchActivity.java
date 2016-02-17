d197a0154c121e3b96678bc42117bc92d55e0ee0
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-17 15:51:32 -0600

    add view parameter of type View to buttons; create and implement handler for runTime() method

--- app/src/main/java/com/example/walter/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/walter/stopwatch/StopwatchActivity.java
@@ -1,6 +1,7 @@
 package com.example.walter.stopwatch;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -19,19 +22,20 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
 
     }
 
-    protected void onClickStart(){
+    protected void onClickStart(View view){
         running = true;
     }
 
-    protected void onClickStop(){
+    protected void onClickStop(View view){
         running = false;
 
     }
 
-    protected void onClickReset(){
+    protected void onClickReset(View view){
         running = false;
         seconds = 0;
 
@@ -40,14 +44,25 @@ public class StopwatchActivity extends AppCompatActivity {
     private void runTimer(){
         //get the TextView
         final TextView timeView = (TextView) findViewById(R.id.time_view);
+        //create a new handler
+        final Handler handler = new Handler();
+        //call the post() method, passing in a new Runnable. The post() method processes codes
+        //without a delay, so the code in the Runnable will run almost immediately
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
 
-        int hours = seconds/3600;
-        int minutes = seconds/60;
-        int secs = seconds%60;
 
-        String time = String.format("%d:%02d:%02d",hours,minutes,secs);
-        timeView.setText(time);
-        if(running) seconds++;
+                int hours = seconds / 3600;
+                int minutes = seconds / 60;
+                int secs = seconds % 60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) seconds++;
+                handler.postDelayed(this, 1000);
+            }
+        });
 
     }
 
